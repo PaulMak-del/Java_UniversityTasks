18ae8bf5bcc58312b3ffc11965435517ae3674c3
My Name <pawelxom@mail.ru>
2021-09-09 18:12:47 +0300

    add lab18 and modify lab3

--- labs/src/ru/mirea/lab3/Movable.java
+++
@@ -1,9 +0,0 @@
-package ru.mirea.lab3;
-
-public abstract class Movable
-{
-    abstract void moveUp();
-    abstract void moveDown();
-    abstract void moveLeft();
-    abstract void moveRight();
-}
